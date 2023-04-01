75b20f0792554aa018351691c589a3502db7c476
Tanwilson18 <totannerwilson@gmail.com>
2023-04-01 01:55:21 -0600

    13

--- frontend/src/Movies.tsx
+++ frontend/src/Movies.tsx
@@ -1,28 +1,41 @@
+import { useState } from 'react';
 import data from './MovieDataSample.json';
 
 const movie = data.MovieDataSample;
 
 function MovieList() {
+  const [listOMovies, setOMovies] = useState(movie);
+
+  const addMovie = () => {
+    setOMovies([
+      ...movie,
+      {
+        Category: 'Action/Adventure',
+        Title: 'Iron Man',
+        Year: 2008,
+        Director: 'Jon Favreau',
+        Rating: 'PG-13',
+      },
+    ]);
+  };
   return (
     <>
       <div>
         <h3>Joe Hilton's Movie Collection</h3>
       </div>
       <div>
-        <table>
+        <table className="table">
           <thead>
             <tr>
-              <th>
-                <th>Title </th>
-                <th>Year</th>
-                <th>Director</th>
-                <th>Rating</th>
-                <th>Category</th>
-              </th>
+              <th>Title </th>
+              <th>Year</th>
+              <th>Director</th>
+              <th>Rating</th>
+              <th>Category</th>
             </tr>
           </thead>
           <tbody>
-            {movie.map((m) => (
+            {listOMovies.map((m) => (
               <tr>
                 <td>{m.Title}</td>
                 <td>{m.Year}</td>
@@ -35,6 +48,11 @@ function MovieList() {
           </tbody>
         </table>
       </div>
+      <button className="btn btn-primary" onClick={addMovie}>
+        Add Movie
+      </button>
     </>
   );
 }
+
+export default MovieList;
