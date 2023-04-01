056806f01620d861f3bbc2f07399d101f893fa74
Tanwilson18 <totannerwilson@gmail.com>
2023-03-31 21:30:22 -0600

    bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-
-function GoButton(props: any) {
-  return (
-    <div>
-      <button onClick={props.click}>G0!</button>
-      <br />
-    </div>
-  );
-}
-
-function AmountGo(props: any) {
-  return <label>{props.num}</label>;
-}
+import TopBanner from './blah';
 
 function App() {
-  const [count, setcount] = useState(0);
-
   return (
-    <div>
-      <GoButton click={() => setcount(count + 1)} />
-      <AmountGo num={count} />
+    <div className="App">
+      <TopBanner />
     </div>
   );
 }
--- frontend/src/blah.tsx
+++ frontend/src/blah.tsx
@@ -1,5 +1,18 @@
-fucntion TopBanner(){
-    return(
-        <img src="./logo109.png" alt="companyLogo" />
-            );
+import React from 'react';
+
+function TopBanner() {
+  return (
+    <>
+      <div className="row">
+        <div className="col-3">
+          <img src="./logo192.png" alt="companyLogo" />
+        </div>
+        <div>
+          <h1>Joe Hilton's movie site</h1>
+        </div>
+      </div>
+    </>
+  );
 }
+
+export default TopBanner;
